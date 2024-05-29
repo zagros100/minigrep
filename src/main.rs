6ff62417dc3d4e35319c56ae8e45872824449d86
zagros100 <hadi_k64f@yahoo.com>
2024-05-29 12:51:39 +0200

    opening the file using filename from args, reading the file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::env;
+use std::{env, fs};
 fn main() {
     let args:Vec<String> = env::args().collect();
     let query = &args[1];
     let filename = &args[2];
     println!("Searching for: {}", query);
     println!("In file: {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
 }
 
