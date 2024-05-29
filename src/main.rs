33059d8f7a7a7bf94b68369a3e0b19ee26082f18
zagros100 <hadi_k64f@yahoo.com>
2024-05-29 14:38:19 +0200

    refactoring and creating run, handlling error, and returning Result type, we need this function for it's side effects, we only care if there is an error, because the type of err is unknwon we use Box dyn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 fn main() {
     let args:Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
@@ -6,9 +6,24 @@ fn main() {
         process::exit(1);
     });
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
+    println!("Searching for '{}' in '{}'",config.query,config.file_path);
+    
+    if let Err(e) = run(config){
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+  
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+
 #[derive(Debug)]
 struct Config{
     query: String,
@@ -20,8 +35,10 @@ impl Config {
         if args.len() < 3{
             return Err("Not enough arguments");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
+
         Ok(Config{query, file_path})
     }
 }
