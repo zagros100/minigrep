5730b41697ac3d7fd7efd732422dc942f049aff3
zagros100 <hadi_k64f@yahoo.com>
2024-05-29 14:51:12 +0200

    seperation of logic and move it into lib.rs, making what's is needed in main.rs public in order to have a modular project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use minigrep::Config;
+
 fn main() {
     let args:Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
@@ -7,39 +10,10 @@ fn main() {
     });
 
     println!("Searching for '{}' in '{}'",config.query,config.file_path);
-    
-    if let Err(e) = run(config){
+
+    if let Err(e) = minigrep::run(config){
         println!("Application error: {e}");
         process::exit(1);
     }
   
 }
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-
-#[derive(Debug)]
-struct Config{
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3{
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config{query, file_path})
-    }
-}
-
