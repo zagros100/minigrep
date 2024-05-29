c54766f210a25e505b59443494087b186deacef6
zagros100 <hadi_k64f@yahoo.com>
2024-05-29 13:27:51 +0200

    creating a constructor for Config struct-the purpose of the parse_config function is to create a Config instance-change parse_config from a plain function to a function named new that is associated with the Config struct. Making this change will make the code more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env, fs};
 fn main() {
     let args:Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text:\n{contents}");
@@ -11,9 +11,12 @@ struct Config{
     query: String,
     file_path: String,
 }
-fn parse_config(args: &[String]) -> Config{
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config{query, file_path}
+
+impl Config {
+    fn new(args: &[String]) -> Config{
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config{query, file_path}
+    }
 }
 
