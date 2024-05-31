c7dd2755401f32e841971d719ef7928747ef8dd7
zagros100 <hadi_k64f@yahoo.com>
2024-05-31 19:20:36 +0200

    using iterator adaptor methods. Doing so also lets us avoid having a mutable intermediate results vector.filter adaptor to keep only the lines that line.contains(query) returns true for. We then collect the matching lines into another vector with collect for both search() and fn insensitive()

--- src/lib.rs
+++ src/lib.rs
@@ -45,26 +45,15 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-    let mut results = Vec::new();
-
-    for line in contents.lines(){
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-    results
-
+    contents.lines()
+    .filter(|line: &&str| line.contains(query))
+    .collect()
 }
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines(){
-        if line.to_lowercase().contains(&query){
-            results.push(line);
-        }
-    }
-    results
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    contents.lines()
+   .filter(|line: &&str| line.to_lowercase().contains(&query.to_lowercase()))
+   .collect()
 
 }
 #[cfg(test)]
