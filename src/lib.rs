9b0dda7fe0d390603c5d39650a278769e027ecbc
zagros100 <hadi_k64f@yahoo.com>
2024-05-31 19:04:11 +0200

    using iterator, directly passing env::args(), which is an iterator and changing the build signature to receive and itertator of any type that implements the Iretator trait and returns a String, calling args.next() to return the next value and consuming -taking ownership- of that value, returninh cohernt err messages

--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,19 @@ pub struct Config{
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3{
-            return Err("Not enough arguments");
-        }
-        
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str>{
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+       
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         
         Ok(Config{query, file_path, ignore_case})
